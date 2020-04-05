b28c62c6c8186a1f9eb4d4fe700f88810577ab84
Clint Geek <clint@clintgeek.com>
2020-04-04 23:11:45 -0500

    move weather to config and script

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -4,6 +4,7 @@
 var settings = {
   "displayClock": true,
   "twelveHourClock": true,
+  "displayWeather": true,
   "displayLinks": true,
   "fancyBackground": true,
 };
@@ -97,6 +98,23 @@ function writeDate() {
 };
 
 // ----------------
+// WEATHER
+// ----------------
+function displayWeather() {
+  if (settings.displayWeather) {
+    $(".weather").append(`
+      <a class="weatherwidget-io" href="https://forecast7.com/en/32d56n97d14/mansfield/?unit=us" data-label_1="MANSFIELD" data-label_2="WEATHER" data-font="Ubuntu" data-theme="dark" >MANSFIELD WEATHER</a>
+      <script>
+        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://weatherwidget.io/js/widget.min.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","weatherwidget-io-js");
+      </script>
+    `);
+  }
+  if (!settings.displayWeather) {
+    $(".weather").css("display", "none");
+  }
+}
+
+// ----------------
 // LINKS
 // ----------------
 if (!settings.displayLinks) {
@@ -184,5 +202,6 @@ function getBackgroundImage() {
 // ----------------
 $(document).ready(function(){
   startClock();
+  displayWeather();
   getBackgroundImage();
 });
